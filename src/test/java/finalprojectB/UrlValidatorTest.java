7b35c347468a70d7fce11693a88f1aa8b8c5a023
DJ <hamblindj@gmail.com>
2018-06-05 18:04:22 -0700

    added a couple manual test

--- src/test/java/finalprojectB/UrlValidatorTest.java
+++ src/test/java/finalprojectB/UrlValidatorTest.java
@@ -1,6 +1,7 @@
 
 package finalprojectB;
 
+import org.junit.Test;
 import junit.framework.TestCase;
 
 //You can use this as a skeleton for your 3 different test approach
@@ -19,12 +20,34 @@ public class UrlValidatorTest extends TestCase {
    }
 
    
-   
+   //this should return true as the url is valid but it does not
+   @Test
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+
+        //
+        String[] schemes = { "http", "https" };
+        UrlValidator urlValidator = new UrlValidator(schemes);
+        assertTrue(urlValidator.isValid("http://tech.yahoo.com/rc/desktops/102;_ylt=Ao8yevQHlZ4On0O3ZJGXLEQFLZA5"));
+ 
    }
+   
+    @Test
+    public void testManualTest_01() {
+
+        
+     UrlValidator urlValidator = new UrlValidator();
+     assertTrue(urlValidator.isValid("ftp://foo.bar.com/"));
+    }
+
+   //this should return true as it is a valid scheme but the assertion fails
+   @Test
+   public void test_isValidScheme(){
+        String[] schemes = { "http", "https" };
+        UrlValidator urlValidator = new UrlValidator(schemes);
+        assertTrue(urlValidator.isValidScheme("http"));
+   }
+
    
    
    public void testYourFirstPartition()
